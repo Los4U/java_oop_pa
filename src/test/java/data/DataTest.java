5823021e65c4169b8ffe15386e36ae40b127d8eb
Los4U <Backup_2018>
2019-04-01 10:58:16 +0200

    implements 3 new MediaItems items for testing , Managers can add and remove MadiaItems from the library

--- src/test/java/data/DataTest.java
+++ src/test/java/data/DataTest.java
@@ -1,16 +1,17 @@
 package data;
 
 import controller.ManagerController;
-import model.Manager;
-import model.MediaItem;
-import model.User;
+import model.*;
+import model.Enums.AlbumGenres;
+import model.Enums.BookGenres;
+import model.Enums.MediaType;
+import model.Enums.MovieGenres;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class DataTest {
 
@@ -20,8 +21,14 @@ class DataTest {
     private ManagerController managerController;
     private List<MediaItem> mediaDatabase = data.getMediaDatabase();
 
+    private MediaItem book = new Book(MediaType.BOOK,"Muminki" ,"Astrid Lindgren" , LocalDate.of(1985, 1 , 1 ),  33, BookGenres.NOVEL );
+    private MediaItem album = new Album(MediaType.ALBUM,"Home by the sea mum" ,"Genesis" ,LocalDate.of(1983, 10 , 3 ) , 45, AlbumGenres.ROCK);
+    private MediaItem itemThatWillBeRemoved = new Movie(MediaType.MOVIE,"Forest Gump" ,"Gorge Lucas" ,LocalDate.of(1990, 10 , 3 ), 112, MovieGenres.COMEDY);
+
     @BeforeEach
     void setUp() {
+
+
     }
 
     @AfterEach
