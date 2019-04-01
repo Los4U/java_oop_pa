d4a438bb0af0cf13475bfcfc9851c3259535cf48
Los4U <Backup_2018>
2019-04-01 10:53:11 +0200

    add private field MovieGenres for Movie class, and use it in constructors

--- src/main/java/model/Movie.java
+++ src/main/java/model/Movie.java
@@ -1,15 +1,19 @@
 package model;
 
 import model.Enums.MediaType;
+import model.Enums.MovieGenres;
 import model.MediaItem;
 import model.interfaces.Watchable;
 
 import java.time.LocalDate;
 
 public class Movie extends MediaItem implements Watchable {
+    private MovieGenres movieGenres;
 
-    public Movie(MediaType mediaType, String title, String author, LocalDate releaseDate, int lenght) {
+    public Movie(MediaType mediaType, String title, String author, LocalDate releaseDate, int lenght, MovieGenres movieGenres) {
         super(mediaType, title, author, releaseDate, lenght);
+        this.movieGenres =  movieGenres;
+
     }
 
     @Override
