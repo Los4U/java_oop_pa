95fda30cc698f08567850ad4863c01515cdfd4f3
Los4U <Backup_2018>
2019-04-01 11:21:22 +0200

    create a method in manager addMediaItemToDatabase, for adding mediaItems int the mediaDatabase, create checkIfMediaToAddIsPropper for checking nulls

--- src/main/java/controller/ManagerController.java
+++ src/main/java/controller/ManagerController.java
@@ -1,6 +1,14 @@
 package controller;
 
+import model.Manager;
+import model.MediaItem;
+
+import java.util.List;
+
 public class ManagerController {
 
 
+    public String addMediaItemToDatabase(List<MediaItem> mediaDatabase, MediaItem mediaItemToAdd, Manager manager) {
+        return manager.addMediaItemToDatabase(mediaDatabase, mediaItemToAdd);
+    }
 }
--- src/main/java/model/Manager.java
+++ src/main/java/model/Manager.java
@@ -1,8 +1,34 @@
 package model;
 
+import com.sun.tools.javac.comp.Todo;
+
+import java.util.List;
+import java.util.function.ToDoubleBiFunction;
+
 public class Manager extends User {
     public Manager(String name) {
         super(name);
+    }
+
+
+    public String addMediaItemToDatabase(List<MediaItem> mediaDatabase, MediaItem mediaItemToAdd) {
+        if(null == mediaItemToAdd){
+            return "Please specify new mediaItem";
+        }else if(!checkIfMediaToAddIsPropper(mediaItemToAdd)){
+            return "Check the submitted date, lack of something.";
+        }
+        mediaDatabase.add(mediaItemToAdd);
+        return "OK";
+    }
+
+    //TODO needs to implements this function more, to check each fields that is not null
+    private boolean checkIfMediaToAddIsPropper(MediaItem mediaItemToAdd) {
+        if(null == mediaItemToAdd.getTitle()
+                || null == mediaItemToAdd.getAuthor()
+                || null == mediaItemToAdd.getReleaseDate()){
+            return false;
+        }
+        return true;
     }
 
 
