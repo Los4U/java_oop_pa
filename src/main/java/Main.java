8d114c0d81431ee46ce98a1c0cf14f5d2c59a8d8
Los4U <Backup_2018>
2019-04-01 11:41:10 +0200

    Add view class, and implement start method in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
+import controller.ManagerController;
+import view.View;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("oop pa");
+
+        ManagerController managerController =  new ManagerController();
+
+        View view =  new View();
+        view.printWelcomeMenu();
+
     }
 }
---
+++ src/main/java/view/View.java
@@ -0,0 +1,10 @@
+package view;
+
+public class View {
+    public View() {
+    }
+
+    public void printWelcomeMenu(){
+        System.out.println("Welcome in CODECOOL Mediatheque");
+    }
+}
